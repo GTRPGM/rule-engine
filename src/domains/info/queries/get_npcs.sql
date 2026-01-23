726da64369fc340e4b920e9a23a5c3518b3e6dde
elian118 <elian118@naver.com>
2026-01-23 16:55:07 +0900

    fix: invalid sql

--- src/domains/info/queries/get_npcs.sql
+++ src/domains/info/queries/get_npcs.sql
@@ -1,4 +1,4 @@
 SELECT * FROM npcs
-WHERE (%(npc_ids)s IS NULL OR item_id = ANY(%(npc_ids)s))
+WHERE (%(npc_ids)s IS NULL OR npc_id = ANY(%(npc_ids)s))
 ORDER BY npc_id ASC
 LIMIT %(limit)s OFFSET %(skip)s;
