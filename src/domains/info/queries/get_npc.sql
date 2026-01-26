032597fc8bb4587e6f2584ea453a4c5f0237cb83
elian118 <elian118@naver.com>
2026-01-26 12:28:11 +0900

    feat: update get npc details query - base_price

--- src/domains/info/queries/get_npc.sql
+++ src/domains/info/queries/get_npc.sql
@@ -5,11 +5,12 @@ SELECT
             json_build_object(
                 'inventory_id', ni.inventory_id,
                 'item_id', ni.item_id,
-                'base_price', ni.base_price,
+                'base_price', i.base_price,
                 'is_infinite_stock', ni.is_infinite_stock
             )
         )
         FROM npc_inventories ni
+        JOIN items i ON ni.item_id = i.item_id
         WHERE ni.npc_id = n.npc_id
     ) AS inventory_list
 FROM npcs n
